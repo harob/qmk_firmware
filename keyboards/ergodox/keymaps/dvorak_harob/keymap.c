16292b90e4b01ec5d0e5ccf31fa9b9ec7874e7e4
Harry <harry.robertson@gmail.com>
2016-08-16 17:27:36 -0700

    Add volume buttons

--- keyboards/ergodox/keymaps/dvorak_harob/keymap.c
+++ keyboards/ergodox/keymaps/dvorak_harob/keymap.c
@@ -24,32 +24,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |  =   |      |AltShf| LAlt | LCmd |                                       | RCmd | RAlt |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        |      |      |       |      |        |
- *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
- *                                 `--------------------'       `----------------------'
+ *                                        | Copy | Paste|       | Vol+ | Vol- |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      | Home |       | PgUp |      |      |
+ *                                 | Space|Backsp|------|       |------| Tab  |Enter |
+ *                                 |      |ace   | End  |       | PgDn |      |      |
+ *                                 `--------------------'       `--------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_GRV,         KC_1,    KC_2,         KC_3,   KC_4,   KC_5,   KC_NO,
-        KC_TAB,         KC_QUOT, KC_COMM,      KC_DOT, KC_P,   KC_Y,   LT(SYMB,KC_NO),
+        KC_GRV,         KC_1,    KC_2,         KC_3,   KC_4,   KC_5,      KC_NO,
+        KC_TAB,         KC_QUOT, KC_COMM,      KC_DOT, KC_P,   KC_Y,      LT(SYMB,KC_NO),
         CTL_T(KC_ESC),  KC_A,    KC_O,         KC_E,   KC_U,   KC_I,
-        KC_LSPO,        KC_SCLN, KC_Q,         KC_J,   KC_K,   KC_X,   ALL_T(KC_NO),
+        KC_LSPO,        KC_SCLN, KC_Q,         KC_J,   KC_K,   KC_X,      ALL_T(KC_NO),
         KC_EQL,         KC_NO,   LALT(KC_LSFT),KC_LALT,KC_LGUI,
-                                                               KC_NO,  KC_NO,
-                                                                       KC_HOME,
-                                                       KC_SPC, KC_BSPC,KC_END,
+                                                               LGUI(KC_C),LGUI(KC_V),
+                                                                          KC_HOME,
+                                                       KC_SPC, KC_BSPC,   KC_END,
         // right hand
         KC_NO,         KC_6,  KC_7,   KC_8,   KC_9,   KC_0,           KC_BSLS,
         LT(SYMB,KC_NO),KC_F,  KC_G,   KC_C,   KC_R,   KC_L,           KC_SLSH,
                        KC_D,  KC_H,   KC_T,   KC_N,   KC_S,           KC_MINS,
         LCAG_T(KC_NO), KC_B,  KC_M,   KC_W,   KC_V,   LT(MDIA, KC_Z), KC_RSPC,
                               KC_RGUI,KC_RALT,KC_LBRC,KC_RBRC,        KC_FN1,
-        KC_NO,         KC_NO,
+        KC_VOLU,       KC_VOLD,
         KC_PGUP,
         KC_PGDN,       KC_TAB,KC_ENT
     ),
