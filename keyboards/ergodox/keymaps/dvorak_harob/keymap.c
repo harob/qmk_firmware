6a90feb410a593ee4c3172e3de993c6392cfd4ce
Harry <harry.robertson@gmail.com>
2016-08-18 14:38:30 -0700

    Reconfigure thumb keys to be symmetrical

--- keyboards/ergodox/keymaps/dvorak_harob/keymap.c
+++ keyboards/ergodox/keymaps/dvorak_harob/keymap.c
@@ -27,22 +27,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                        | Copy | Paste|       | Vol- | Vol+ |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      | Home |       | PgUp |      |      |
- *                                 | Space|  ~L1 |------|       |------|Enter |Space |
- *                                 |      |      | End  |       | PgDn |      |      |
+ *                                 | Space| Enter|------|       |------|Enter |Space |
+ *                                 |  /L1 |      | End  |       | PgDn |      | /L1  |
  *                                 `--------------------'       `--------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_GRV,         KC_1,    KC_2,    KC_3,   KC_4,   KC_5,      KC_NO,
-        KC_TAB,         KC_QUOT, KC_COMM, KC_DOT, KC_P,   KC_Y,      ALL_T(KC_NO),
-        CTL_T(KC_ESC),  KC_A,    KC_O,    KC_E,   KC_U,   KC_I,
-        KC_LSPO,        KC_SCLN, KC_Q,    KC_J,   KC_K,   KC_X,      LT(SYMB,KC_NO),
+        KC_GRV,         KC_1,    KC_2,    KC_3,   KC_4,           KC_5,      KC_NO,
+        KC_TAB,         KC_QUOT, KC_COMM, KC_DOT, KC_P,           KC_Y,      ALL_T(KC_NO),
+        CTL_T(KC_ESC),  KC_A,    KC_O,    KC_E,   KC_U,           KC_I,
+        KC_LSPO,        KC_SCLN, KC_Q,    KC_J,   KC_K,           KC_X,      LT(SYMB,KC_NO),
         KC_EQL,         KC_NO,   KC_LCTL, KC_LALT,KC_LGUI,
-                                                          LGUI(KC_C),LGUI(KC_V),
-                                                                     KC_HOME,
-                                                  KC_SPC, KC_FN1,    KC_END,
+                                                                  LGUI(KC_C),LGUI(KC_V),
+                                                                             KC_HOME,
+                                                  LT(SYMB,KC_SPC),KC_ENT,    KC_END,
         // right hand
         KC_NO,         KC_6,  KC_7,   KC_8,   KC_9,  KC_0,           KC_BSLS,
         LCAG_T(KC_NO), KC_F,  KC_G,   KC_C,   KC_R,  KC_L,           KC_SLSH,
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                               KC_RGUI,KC_RALT,KC_NO, KC_NO,          KC_FN1,
         KC_VOLD,       KC_VOLU,
         KC_PGUP,
-        KC_PGDN,       KC_ENT,KC_SPC
+        KC_PGDN,       KC_ENT,LT(SYMB,KC_SPC)
     ),
 /* Keymap 1: Symbol Layer
  *
